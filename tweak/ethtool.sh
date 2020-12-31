2246fa92543b9dd72dc7d0c8a0227128cc076441
bate <25872749+batedk@users.noreply.github.com>
2021-01-01 00:14:44 +0100

    Add files via upload

---
+++ tweak/ethtool.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+echo "Ethtool service started at ${DATE}" | systemd-cat -p info
+for nic in $(ifconfig | cut -d':' -f1 | grep -v ' ' | grep -v -e '^$'); do
+	ethtool -G $nic rx 4096 2>&1
+	ethtool -G $nic tx 4096 2>&1
+done
+# Looping to keep the service alive
+while :; do
+	echo "Service looping..."
+	sleep 30
+done
