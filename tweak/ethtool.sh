6ff05da3ba829b91779cf9eb00ca592dc336244c
bate <25872749+batedk@users.noreply.github.com>
2021-01-01 00:04:03 +0100

    Update ethtool.sh

--- tweak/ethtool.sh
+++ tweak/ethtool.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo "SWARM's Ethtool service started at ${DATE}" | systemd-cat -p info
+echo "Ethtool service started at ${DATE}" | systemd-cat -p info
 for nic in $(ifconfig | cut -d':' -f1 | grep -v ' ' | grep -v -e '^$'); do
 	ethtool -G $nic rx 4096 2>&1
 	ethtool -G $nic tx 4096 2>&1
